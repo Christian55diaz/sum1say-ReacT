36c61138ec9343d885697602b5534f098c925a9a
Christian Diaz <christian55diaz@gmail.com>
2022-03-08 05:04:55 -0500

    intro for the left side done

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,33 @@
+   import "./intro.css"
+   
+   const Intro = () => {
+        return (
+            //i = shortcut for index
+            <div className="i">
+                <div className="i-left">
+                    <div className="i-left-wrapper">
+                        <h2 className="i-welcome">Welcome, My name is</h2>
+                        <h1 className="i-gov">Christian Diaz</h1>
+                        <div className="i-title">
+                           <div className="i-title-wrapper">
+                                <div className="i-title-career">Full Stack Web Developer
+                                </div>
+                               </div>
+                               <div className="i-about-me">
+                                   I over the six months being at the Columbia Enginering bootcamp
+                                   have worked on over twenty different projects over various topics 
+                                   and have worked on three group projects building full stack websites.
+
+                                   This schooling is my step towards the Software Enginering feild, where I will
+                                   grow and develop more to hopefully create that game changing app!
+
+                                   </div>
+                            </div>
+                        </div>
+                    </div>
+                <div className="i-right">right</div>
+            </div>
+        )
+    }
+
+    export default Intro
