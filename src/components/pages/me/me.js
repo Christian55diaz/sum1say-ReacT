73e14039a740f3db1fde69baadfe0c27147ad232
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 08:18:06 -0400

    about me js finished

---
+++ src/components/pages/me/me.js
@@ -0,0 +1,24 @@
+// importing
+import React from "react";
+import "./me.css";
+
+//info about me function
+function Me() {
+    return(
+        <div className="me-container" id="Me">
+        <div>
+            <h1 className="me-title">About me</h1>
+        </div>
+        <section className="me-container">
+        <p>
+        Welcome to my portfolio! My name is Christian Diaz. I over the six months being at the Columbia Enginering bootcamp have worked on over 
+        twenty different projects over various topics and have worked on three group projects building full stack websites.This schooling is my 
+        foundation and step towards the Software Enginering feild, where I will grow and develop more to hopefully create that game changing app!
+        I have skills in Javascript, Html, CSS, MySQL, MongoDB, Mongoose, and Communication.
+        </p>
+        </section>
+        </div>
+    );
+}
+
+export default Me;
