426399d0cd244ff0d3463d0f784fb30019a44bbf
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 11:34:47 -0400

    finished project data

--- src/components/pages/work/workData.js
+++ src/components/pages/work/workData.js
@@ -7,4 +7,28 @@ export const projects = [
         image: "/Assets/Quiztime.png",
         link: "https://github.com/Christian55diaz/Quiztime",
       },
-]
+      {
+        title: "Budgte Tracker",
+        subtitle: "Manifest and Javascript ",
+        description:
+          "Ever want to track your cash? Use this",
+        image: "/Assets/budgettracker.png",
+        link: "https://github.com/Christian55diaz/Tracking.cash",
+      },
+      {
+        title: "E Commerce Backend",
+        subtitle: "Sequalize and mySQL",
+        description:
+          "Only the backend of an e commerce website",
+        image: "/Assets/ecommerce.png",
+        link: "https://github.com/Christian55diaz/E-commerce-Back-End",
+      },
+      {
+        title: "Social Network Api",
+        subtitle: "Javascript",
+        description:
+          "Social media api that allows you to find specific id of comments, users or posts",
+        image: "/Assets/socnetwork.png",
+        link: "https://github.com/Christian55diaz/Soc.Network-Api",
+      },
+];
