ed0418ab394e68d834d103b9fbe75f783b1add05
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 07:44:12 -0400

    navbar return function

--- src/components/intro/Navbar.jsx
+++ src/components/intro/Navbar.jsx
@@ -9,3 +9,25 @@ function Navbar() {
       if (active === "nav__menu") {
         setActive("nav__menu nav__active");
       } else setActive("nav__menu");
+    //   setup the return call function
+    // navbar list
+    return (
+          <ul className={active}>
+            <li className="nav__item">
+              <a href="#" className="nav__link">
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#" className="nav__link">
+                About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#" className="nav__link">
+                Contact
+              </a>
+            </li>
+          </ul>
+    )
+}
