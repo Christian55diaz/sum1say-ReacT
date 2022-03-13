9ce41107e3a2d04441943ecef6b2279b42ae1c82
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 07:58:27 -0400

    index.css initial

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+
+
+const App = () => {
+  return (<div><Intro/></div>);
+};
+
+export default App;
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Intro from "./components/intro/Intro";
-import 'bootstrap/dist/css/bootstrap.css';
-
-
-const App = () => {
-  return (<div><Intro/></div>);
-};
-
-export default App;
