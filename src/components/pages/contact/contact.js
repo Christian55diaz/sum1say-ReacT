c25729788805d15bcbf6a431ac452771be1c414a
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 11:45:03 -0400

    setting up contact form

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -18,6 +18,19 @@ export default function Contact() {
               </div>
           </div>
         </div>
+        <form
+        // start of contact me form
+        netlify
+        name="contact"
+        className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                Contact Me
+                </h2>
+                <p className="leading-relaxed mb-5">
+                    Fill out the fields below if you would like to get in contact with me.
+                    </p>
+            
+            </form>
 </section> 
     )
 }
