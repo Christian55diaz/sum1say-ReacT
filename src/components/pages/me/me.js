e87093519cac41973e322ed420f2c5625b6caedb
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 10:52:43 -0400

    changing around how the about me seciton looks

--- src/components/pages/me/me.js
+++ src/components/pages/me/me.js
@@ -5,10 +5,15 @@ import React from "react";
 //export and calling function
 export default function Me() {
     return(
+        //adding css into the js file instead of using a css file
         <section id="about">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Welcome, I'm Christian.
+                {/* we put a break so there is room for this statement */}
+                <br className="hidden lg:inline-block" /> Full Stack Web Developer
+          </h1>
         <div>
             <h1 className="me-title">About me</h1>
         </div>
