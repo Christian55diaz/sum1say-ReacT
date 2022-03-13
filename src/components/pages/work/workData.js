8927ab704d32f2e7853804735b05a41963ec4bac
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 11:28:11 -0400

    first project added

---
+++ src/components/pages/work/workData.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        title: "Quiz Time",
+        subtitle: "Javascript",
+        description:
+          "Quiz app using basic html, css and js.",
+        image: "/Assets/Quiztime.png",
+        link: "https://github.com/Christian55diaz/Quiztime",
+      },
+]
