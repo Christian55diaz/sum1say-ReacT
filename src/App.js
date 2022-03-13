0ba5281de5847bf8a37cbfd5c16dc0f1af3acc99
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 08:01:34 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-
-
+// importing all of what i neeed
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import AboutMe from "./components/pages/me/me";
+import MyWork from "./components/pages/work/work";
+import ContactMe from "./components/pages/contact-me/contact-me";
 
 const App = () => {
   return (<div><Intro/></div>);
