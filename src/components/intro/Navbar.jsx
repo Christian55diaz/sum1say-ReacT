6b4939e8f0424cde980c20b113513acf21319a9c
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 07:40:35 -0400

    navbar function

--- src/components/intro/Navbar.jsx
+++ src/components/intro/Navbar.jsx
@@ -1,3 +1,11 @@
 // imports
 import React, { useState } from "react";
 import "../Navbar.css";
+
+// navbar function
+function Navbar() {
+    const [active, setActive] = useState("nav__menu");
+    const navToggle = () => {
+      if (active === "nav__menu") {
+        setActive("nav__menu nav__active");
+      } else setActive("nav__menu");
