f4e49e7be796380c54ce1d726e5c3e7ec7dd7794
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 08:21:27 -0400

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // importing all of what i neeed
-import "./App.css";
+import "./app.css";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import AboutMe from "./components/pages/me/me";
 import MyWork from "./components/pages/work/work";
-import ContactMe from "./components/pages/contact-me/contact-me";
+import ContactMe from "./components/pages/contactme/contact-me";
 
 function App() {
   return (
