dbdbac6c9df134b016ab1da38de7661cdf97055e
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 08:56:44 -0400

    adding responsive navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,13 +2,22 @@
     import './navbar.css' 
     
     export default function navbar() {
+        const [toggleMenu, setToggleMenu] = useState(false)
+        const toggleNav = () => {
+            // !reverses my false use state
+            setToggleMenu(!toggleMenu)
+        }
+
       return (
         <nav>
-            <ul className='list'>
-            <li className='items'>About Me</li>
-            <li className='items'>Projects</li>
-            <li className='items'>Contact</li>
-            </ul>
+            {toggleMenu && (
+                <ul className='list'>
+                <li className='items'>About Me</li>
+                <li className='items'>Projects</li>
+                <li className='items'>Contact</li>
+                </ul>
+            )}
+            <button className='btn'>click me</button>
         </nav>
       )
     }
