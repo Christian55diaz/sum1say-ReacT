62b374d1ead29d2996cb1c64cdd942689b40bd72
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 11:17:14 -0400

    project mapping complete

--- src/components/pages/work/work.js
+++ src/components/pages/work/work.js
@@ -24,5 +24,27 @@ export default function Projects() {
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative"></div>
               {/* then we grab a specific picture */}
-
-        </section>
+              <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={project.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                      {/* if project has subtitle add it */}
+                    {project.subtitle}
+                  </h2>
+                  {/* project title */}
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  {/* project description */}
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
