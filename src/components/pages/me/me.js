19e3639e9d365748dba0275e03dc4a56f335b963
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 20:16:01 -0400

    refractored me.js

--- src/components/pages/me/me.js
+++ src/components/pages/me/me.js
@@ -7,14 +7,12 @@ export default function Me() {
     return(
         //adding css into the js file instead of using a css file
         <section id="Me">
-            <div className=""></div>
-            <div className=""></div>
             <h1 className="title">
                 Welcome, I'm Christian.
                 {/* we put a break so there is room for this statement */}
                 <br className="" /> Full Stack Web Developer
           </h1>
-          <p className="">
+          <p className="about-text">
         Welcome to my portfolio! My name is Christian Diaz. I over the six months being at the Columbia Enginering bootcamp have worked on over 
         twenty different projects over various topics and have worked on three group projects building full stack websites.This schooling is my 
         foundation and step towards the Software Enginering feild, where I will grow and develop more to hopefully create that game changing app!
